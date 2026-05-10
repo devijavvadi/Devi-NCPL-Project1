776683bdbb1d29a14041032fe0c24f013cc65119
devijavvadi <devi.javvadi@gmail.com>
2026-05-10 01:02:40 -0400

    ,,,,

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -5,3 +5,23 @@ output "ec2_instance_id" {
 output "ec2_public_ip" {
   value = module.ec2.public_ip
 }
+
+output "s3_bucket_name" {
+  value = module.s3.bucket_name
+}
+
+output "s3_bucket_arn" {
+  value = module.s3.bucket_arn
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
